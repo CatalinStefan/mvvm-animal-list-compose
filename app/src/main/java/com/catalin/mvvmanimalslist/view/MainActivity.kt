f016f49a48a32c2c946a391a676e1e32ed6d2fb1
cata <catalinstefan@Catalins-MacBook-Pro.local>
2022-09-13 17:18:08 +0100

    Activity cleanup

--- app/src/main/java/com/catalin/mvvmanimalslist/view/MainActivity.kt
+++ app/src/main/java/com/catalin/mvvmanimalslist/view/MainActivity.kt
@@ -2,7 +2,6 @@ package com.catalin.mvvmanimalslist.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
 import com.catalin.mvvmanimalslist.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,9 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
     }
 }
